b8d9885a250b22a729782b85887eaa0aba9efbd3
Mr.Resor <jakusienko@gmail.com>
2024-10-05 13:37:57 +0200

    change text on main page, add navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { NavLinks } from '@/app/ui/nav-links'
 import localFont from "next/font/local";
 import "./globals.css";
 
@@ -28,7 +29,8 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <NavLinks />
+        <main>{children}</main>
       </body>
     </html>
   );
